db7e8274ad6d99bb4a943daa6eb94828026683fd
programgames <julgamer1@gmail.com>
2021-09-27 22:28:38 +0200

    update regex title

--- handler/pokemonmovehandler.py
+++ handler/pokemonmovehandler.py
@@ -7,6 +7,7 @@ import os
 
 load_dotenv()
 
+
 def process_pokemon_move(start: int, gen: int, only_download=False):
     logging.basicConfig(filename=os.getenv('LOG_PATH'))
     pokemons = find_pokemon_with_specific_page(start)
@@ -19,12 +20,11 @@ def process_pokemon_move(start: int, gen: int, only_download=False):
 
     for id, pokemon in pokemons.items():
         for generation in generations:
-            # try:
-            print('processing ' + pokemon.identifier + ' for generation ' + str(
-                generation.id) + f" with id {pokemon.id}")
-            pokemonmoveprocessor.process(generation, learnmethod, pokemon, False, only_download)
+            try:
+                print('processing ' + pokemon.identifier + ' for generation ' + str(
+                    generation.id) + f" with id {pokemon.id}")
+                pokemonmoveprocessor.process(generation, learnmethod, pokemon, False, only_download)
 
-            # except Exception as exc:
-            #     logging.error("Error happened for {} generation {} , error : {}".format(pokemon.identifier,
-            #                                                                generation.identifier,exc))
-
+            except Exception as exc:
+                logging.error("Error happened for {} generation {} , error : {}".format(pokemon.identifier,
+                                                                           generation.identifier,exc))
--- satanizer/pokepedialevelmovesatanizer.py
+++ satanizer/pokepedialevelmovesatanizer.py
@@ -14,7 +14,7 @@ def check_and_sanitize_moves(moves: list, pokemon_name: str) -> dict:
         'forms': OrderedDict(),
         'botComments': [],
     }
-    if not re.search(r'(\[\[Septième génération]])|(\[\[Huitième génération]])|(\[\[Par montée en \[\[niveau]]]])',
+    if not re.search(r'(\[\[Septième génération]])|(\[\[Huitième génération]])|(Par montée en \[\[niveau]])',
                      moves[0]):
         raise WrongHeaderError('Invalid header: {}'.format(moves[0]))
     section['topComments'].append(moves[0])
